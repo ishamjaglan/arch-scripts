370454c3cf14e63287df236e5f6d0a9d4a7f42f9
Isham Jaglan <48505790+ixh4m@users.noreply.github.com>
2022-02-20 22:47:50 +0530

    Update setup.sh

--- hacking-setup/setup.sh
+++ hacking-setup/setup.sh
@@ -11,7 +11,7 @@ sudo pacman -S base-devel --noconfirm
 sudo pacman -S yay --noconfirm
 echo -e "\n\nInstalling Security Tools"
 sudo pacman -S nmap gobuster ffuf smbclient metasploit binwalk wireshark-qt john hydra netcat ghidra macchanger wpscan seclists haiti burpsuite aircrack-ng bettercap bettercap-ui hashcat hexedit tor torsocks torbrowser-launcher sqlmap maltego nikto recon-ng phoneinfoga audacity exiv2 nipe steghide stegsolve zaproxy android-apktool dex2jar autopsy snort --noconfirm
-yay -S zsteg --noconfirm
+yay -S zsteg remmina --noconfirm
 echo -e "\n\n"
 figlet Script made by Isham Jaglan ixh4m
 echo "https://github.com/ixh4m"
